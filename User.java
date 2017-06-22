beb5aeac840f660de2a151abee7ced4a1c0b054f
Krishna Manvar <krishnamanvar35@gmail.com>
2017-06-22 22:36:15 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,32 @@
+
+public class User {
+	private String name;
+	private int password;
+	private int budget;
+	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPassword() {
+		return password;
+	}
+	public void setPassword(int password) {
+		this.password = password;
+	}
+	public int getBudget() {
+		return budget;
+	}
+	public void setBudget(int budget) {
+		this.budget = budget;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
