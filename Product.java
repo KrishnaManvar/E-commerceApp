b78799f0d92869e7b7d46942c1026ccc76b39e45
Krishna Manvar <krishnamanvar35@gmail.com>
2017-06-22 22:33:34 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,32 @@
+
+public class Product {
+	private String name;
+	private int price;
+	private String category;
+	private int popularity;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public int getPopularity() {
+		return popularity;
+	}
+	public void setPopularity(int popularity) {
+		this.popularity = popularity;
+	}
+	
+}
